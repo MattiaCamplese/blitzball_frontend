447d4aa2bf990398b275e9a740232003497fb690
MattiaCamplese <Mattia1996@gmail.com>
2026-04-20 20:55:39 +0200

    fix tournamet

--- src/features/bracket/BracketConnector.tsx
+++ src/features/bracket/BracketConnector.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { Game } from '../game/game.type';
 import type { BracketColumn } from './bracket.utils';
 
